80c2352cca8e344f1cfde21bf357ca81cdc06152
GLGIO7 <giovanelucas00@outlook.com>
2023-04-27 03:33:56 -0300

    chore: some tests with api

--- server/src/index.ts
+++ server/src/index.ts
@@ -7,9 +7,33 @@ const port = process.env.PORT;
 const app = express();
 const router = express.Router();
 
+const users = [
+	{
+		name: "Alice",
+		email: "alice@example.com",
+		age: 27,
+		city: "New York",
+	},
+	{
+		name: "John Doe",
+		email: "bob@example.com",
+		age: 34,
+		city: "San Francisco",
+	},
+	{
+		name: "Jane Doe",
+		email: "charlie@example.com",
+		age: 19,
+		city: "Los Angeles",
+	},
+];
+
 router.get("/", (req, res) => {
-	alert("Api em construção");
 	res.status(204);
+});
+
+router.get("/users", (req, res) => {
+	res.status(200).json(users);
 });
 
 app.use("/api", router);
