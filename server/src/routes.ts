4d4620e2c283c0e38f4ad7c5813a153b8f14ebe9
GLGIO7 <giovanelucas00@outlook.com>
2023-05-09 15:24:41 -0300

    add: route to auth

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -3,29 +3,42 @@ import { GetUsersController } from "./controllers/get-users/get-users";
 import { MongoGetUsersRepository } from "./repositories/get-users/mongo-get-users";
 import { MongoCreateUserRepository } from "./repositories/create-user/mongo-create-user";
 import { CreateUserController } from "./controllers/create-user/create-user";
+import { MongoAuthUserRepository } from "./repositories/auth-user/mongo-auth-user";
+import { AuthUserController } from "./controllers/auth-user/auth-user";
 
 export const createRoutes = () => {
-  const router = Router();
+	const router = Router();
 
-  router.get("/users", async (req, res) => {
-    const getUsersRepository = new MongoGetUsersRepository();
-    const getUsersController = new GetUsersController(getUsersRepository);
+	router.get("/users", async (req, res) => {
+		const getUsersRepository = new MongoGetUsersRepository();
+		const getUsersController = new GetUsersController(getUsersRepository);
 
-    const response = await getUsersController.handle();
+		const response = await getUsersController.handle();
 
-    res.send(response.body).status(response.statusCode);
-  });
+		res.send(response.body).status(response.statusCode);
+	});
 
-  router.post("/users", async (req, res) => {
-    const createUserRepository = new MongoCreateUserRepository();
-    const createUserController = new CreateUserController(createUserRepository);
+	router.post("/users", async (req, res) => {
+		const createUserRepository = new MongoCreateUserRepository();
+		const createUserController = new CreateUserController(createUserRepository);
 
-    const { body, statusCode } = await createUserController.handle({
-      body: req.body,
-    });
+		const { body, statusCode } = await createUserController.handle({
+			body: req.body,
+		});
 
-    res.send(body).status(statusCode);
-  });
+		res.send(body).status(statusCode);
+	});
 
-  return router;
+	router.post("/auth", async (req, res) => {
+		const authUserRepository = new MongoAuthUserRepository();
+		const authUserController = new AuthUserController(authUserRepository);
+
+		const { body, statusCode } = await authUserController.handle({
+			body: req.body,
+		});
+
+		res.send(body).status(statusCode);
+	});
+
+	return router;
 };
