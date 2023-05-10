2dd42cf6cd99e0e0e0ad9cb8ccee9b193a5b6536
GLGIO7 <giovanelucas00@outlook.com>
2023-05-10 19:50:25 -0300

    feat: add cors

--- server/src/index.ts
+++ server/src/index.ts
@@ -11,7 +11,7 @@ const main = async () => {
 	const app = express();
 
 	app.use(express.json());
-	app.use(cors());
+	app.use(cors({ origin: "https://watchous.netlify.app" }));
 	app.use("/", createRoutes());
 
 	await MongoClient.connect();
