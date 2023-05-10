5ef69eca4edd34c25b1ca5dfefd75e7a738612f5
GLGIO7 <giovanelucas00@outlook.com>
2023-05-10 20:56:13 -0300

    chore: restoring scripts

--- server/src/index.ts
+++ server/src/index.ts
@@ -7,7 +7,7 @@ import { createRoutes } from "./routes";
 const main = async () => {
 	dotenv.config();
 
-	const port = process.env.BACKEND_PORT || 8000;
+	const port = process.env.PORT || 3333;
 	const app = express();
 
 	app.use(express.json());
